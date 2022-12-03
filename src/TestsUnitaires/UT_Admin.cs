03e59ac892dc68d0e098ee247b9338c00e45008f
Baptiste | nojii_ <68440638+Baptiiiiste@users.noreply.github.com>
2022-12-03 23:30:22 +0100

    Update UT_Admin.cs

--- src/TestsUnitaires/UT_Admin.cs
+++ src/TestsUnitaires/UT_Admin.cs
@@ -8,35 +8,35 @@ namespace TestsUnitaires
     public class UT_Admin
     {
         [Theory]
-        [InlineData("Bonneau", "Baptiste", "baptiste.bonneau@etu.uca.fr", "0612345678", "37 rue thisIsMonAdresse", "Clermont-Ferrand", "63000", "password", "NewPizza()",
-                    "Bonneau", "Baptiste", "baptiste.bonneau@etu.uca.fr", "0612345678", "37 rue thisIsMonAdresse", "Clermont-Ferrand", "63000", "password", "NewPizza()")] // tout bon
+        [InlineData("x", "Baptiste", "xxxx@etu.uca.fr", "0612345678", "37 rue thisIsMonAdresse", "Clermont-Ferrand", "63000", "password", "NewPizza()",
+                    "x", "Baptiste", "xxxx@etu.uca.fr", "0612345678", "37 rue thisIsMonAdresse", "Clermont-Ferrand", "63000", "password", "NewPizza()")] // tout bon
 
-        [InlineData("", "Baptiste", "baptiste.bonneau@etu.uca.fr", "0612345678", "37 rue thisIsMonAdresse", "Clermont-Ferrand", "63000", "password", "NewPizza()",
-                    "Non-renseigné", "Baptiste", "baptiste.bonneau@etu.uca.fr", "0612345678", "37 rue thisIsMonAdresse", "Clermont-Ferrand", "63000", "password", "NewPizza()")] // Prenom vide
+        [InlineData("", "Baptiste", "xxxx@etu.uca.fr", "0612345678", "37 rue thisIsMonAdresse", "Clermont-Ferrand", "63000", "password", "NewPizza()",
+                    "Non-renseigné", "Baptiste", "xxxx@etu.uca.fr", "0612345678", "37 rue thisIsMonAdresse", "Clermont-Ferrand", "63000", "password", "NewPizza()")] // Prenom vide
 
-        [InlineData("Bonneau", "", "baptiste.bonneau@etu.uca.fr", "0612345678", "37 rue thisIsMonAdresse", "Clermont-Ferrand", "63000", "password", "NewPizza()",
-                    "Bonneau", "Non-renseigné", "baptiste.bonneau@etu.uca.fr", "0612345678", "37 rue thisIsMonAdresse", "Clermont-Ferrand", "63000", "password", "NewPizza()")] // Nom vide
+        [InlineData("x", "", "xxxx@etu.uca.fr", "0612345678", "37 rue thisIsMonAdresse", "Clermont-Ferrand", "63000", "password", "NewPizza()",
+                    "x", "Non-renseigné", "xxxx@etu.uca.fr", "0612345678", "37 rue thisIsMonAdresse", "Clermont-Ferrand", "63000", "password", "NewPizza()")] // Nom vide
 
-        [InlineData("Bonneau", "Baptiste", "", "0612345678", "37 rue thisIsMonAdresse", "Clermont-Ferrand", "63000", "password", "NewPizza()",
-                    "Bonneau", "Baptiste", "", "0612345678", "37 rue thisIsMonAdresse", "Clermont-Ferrand", "63000", "password", "NewPizza()")] // Email vide
+        [InlineData("x", "Baptiste", "", "0612345678", "37 rue thisIsMonAdresse", "Clermont-Ferrand", "63000", "password", "NewPizza()",
+                    "x", "Baptiste", "", "0612345678", "37 rue thisIsMonAdresse", "Clermont-Ferrand", "63000", "password", "NewPizza()")] // Email vide
 
-        [InlineData("Bonneau", "Baptiste", "baptiste.bonneau@etu.uca.fr", "", "37 rue thisIsMonAdresse", "Clermont-Ferrand", "63000", "password", "NewPizza()",
-                    "Bonneau", "Baptiste", "baptiste.bonneau@etu.uca.fr", "Non-renseigné", "37 rue thisIsMonAdresse", "Clermont-Ferrand", "63000", "password", "NewPizza()")] // telephone vide
+        [InlineData("x", "Baptiste", "xxxx@etu.uca.fr", "", "37 rue thisIsMonAdresse", "Clermont-Ferrand", "63000", "password", "NewPizza()",
+                    "x", "Baptiste", "xxxx@etu.uca.fr", "Non-renseigné", "37 rue thisIsMonAdresse", "Clermont-Ferrand", "63000", "password", "NewPizza()")] // telephone vide
 
-        [InlineData("Bonneau", "Baptiste", "baptiste.bonneau@etu.uca.fr", "0612345678", "", "Clermont-Ferrand", "63000", "password", "NewPizza()",
-                    "Bonneau", "Baptiste", "baptiste.bonneau@etu.uca.fr", "0612345678", "Non renseignée", "Clermont-Ferrand", "63000", "password", "NewPizza()")] // Adresse vide
+        [InlineData("x", "Baptiste", "xxxx@etu.uca.fr", "0612345678", "", "Clermont-Ferrand", "63000", "password", "NewPizza()",
+                    "x", "Baptiste", "xxxx@etu.uca.fr", "0612345678", "Non renseignée", "Clermont-Ferrand", "63000", "password", "NewPizza()")] // Adresse vide
 
-        [InlineData("Bonneau", "Baptiste", "baptiste.bonneau@etu.uca.fr", "0612345678", "37 rue thisIsMonAdresse", "", "63000", "password", "NewPizza()",
-                    "Bonneau", "Baptiste", "baptiste.bonneau@etu.uca.fr", "0612345678", "37 rue thisIsMonAdresse", "Non-renseigné", "63000", "password", "NewPizza()")] // Ville vide
+        [InlineData("x", "Baptiste", "xxxx@etu.uca.fr", "0612345678", "37 rue thisIsMonAdresse", "", "63000", "password", "NewPizza()",
+                    "x", "Baptiste", "xxxx@etu.uca.fr", "0612345678", "37 rue thisIsMonAdresse", "Non-renseigné", "63000", "password", "NewPizza()")] // Ville vide
 
-        [InlineData("Bonneau", "Baptiste", "baptiste.bonneau@etu.uca.fr", "0612345678", "37 rue thisIsMonAdresse", "Clermont-Ferrand", "", "password", "NewPizza()",
-                    "Bonneau", "Baptiste", "baptiste.bonneau@etu.uca.fr", "0612345678", "37 rue thisIsMonAdresse", "Clermont-Ferrand", "00000", "password", "NewPizza()")] // CodePostal vide
+        [InlineData("x", "Baptiste", "xxxx@etu.uca.fr", "0612345678", "37 rue thisIsMonAdresse", "Clermont-Ferrand", "", "password", "NewPizza()",
+                    "x", "Baptiste", "xxxx@etu.uca.fr", "0612345678", "37 rue thisIsMonAdresse", "Clermont-Ferrand", "00000", "password", "NewPizza()")] // CodePostal vide
 
-        [InlineData("Bonneau", "Baptiste", "baptiste.bonneau@etu.uca.fr", "0612345678", "37 rue thisIsMonAdresse", "Clermont-Ferrand", "63000", "password", "",
-                    "Bonneau", "Baptiste", "baptiste.bonneau@etu.uca.fr", "0612345678", "37 rue thisIsMonAdresse", "Clermont-Ferrand", "63000", "password", "")] // nomPizzeria vide
+        [InlineData("x", "Baptiste", "xxxx@etu.uca.fr", "0612345678", "37 rue thisIsMonAdresse", "Clermont-Ferrand", "63000", "password", "",
+                    "x", "Baptiste", "xxxx@etu.uca.fr", "0612345678", "37 rue thisIsMonAdresse", "Clermont-Ferrand", "63000", "password", "")] // nomPizzeria vide
 
-        [InlineData("Bonneau", "Baptiste", "baptiste.bonneau@etu.uca.fr", "0612345678", "37 rue thisIsMonAdresse", "Clermont-Ferrand", "63000", "", "NewPizza()",
-                    "Bonneau", "Baptiste", "baptiste.bonneau@etu.uca.fr", "0612345678", "37 rue thisIsMonAdresse", "Clermont-Ferrand", "63000", "1234", "NewPizza()")] // Mdp vide
+        [InlineData("x", "Baptiste", "xxxx@etu.uca.fr", "0612345678", "37 rue thisIsMonAdresse", "Clermont-Ferrand", "63000", "", "NewPizza()",
+                    "x", "Baptiste", "xxxx@etu.uca.fr", "0612345678", "37 rue thisIsMonAdresse", "Clermont-Ferrand", "63000", "1234", "NewPizza()")] // Mdp vide
         public void TEST_Ctor(string nom, string prénom, string email, string téléphone, string adresse, string ville, string codePostal, string mdp, string nomPizzeria,
                               string expNom, string expPrénom, string expEmail, string expTéléphone, string expAdresse, string expVille, string expCodePostal, string expMdp, string expNomPizzeria)
         {
@@ -57,7 +57,7 @@ namespace TestsUnitaires
         [Fact]
         public void TEST_ChangerStatusCommande()
         {
-            Client c1 = new Client("Bonneau", "Baptiste", "baptiste.bonneau@etu.uca.fr", "0606060606", "37 rue mon adresse", "clermont", "password", "pseudo", "photo.png");
+            Client c1 = new Client("x", "Baptiste", "xxxx@etu.uca.fr", "0606060606", "37 rue mon adresse", "clermont", "password", "pseudo", "photo.png");
             List<Ingredients> listIngre = new List<Ingredients> { (Ingredients)55, (Ingredients)13, (Ingredients)42, (Ingredients)21, (Ingredients)36 };
             Pizza p = new Pizza("Nom", "Description", "Image", listIngre, 8);
             Pizza p2 = new Pizza("Nom2", "Description2", "Image2", listIngre, 8);
@@ -88,7 +88,7 @@ namespace TestsUnitaires
         [Fact]
         public void TEST_SuppCommande()
         {
-            Client c1 = new Client("Bonneau", "Baptiste", "baptiste.bonneau@etu.uca.fr", "0606060606", "37 rue mon adresse", "clermont", "password", "pseudo", "photo.png");
+            Client c1 = new Client("x", "Baptiste", "xxxx@etu.uca.fr", "0606060606", "37 rue mon adresse", "clermont", "password", "pseudo", "photo.png");
             List<Ingredients> listIngre = new List<Ingredients> { (Ingredients)55, (Ingredients)13, (Ingredients)42, (Ingredients)21, (Ingredients)36 };
             Pizza p = new Pizza("Nom", "Description", "Image", listIngre, 8);
             Pizza p2 = new Pizza("Nom2", "Description2", "Image2", listIngre, 8);
